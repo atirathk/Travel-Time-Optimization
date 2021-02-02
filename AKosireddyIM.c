25a3f4eacd47dd7d71f76ad0671d7e8c2082890a
Atirath Kosireddy <atikosireddy98@gmail.com>
2021-02-02 10:39:52 -0800

    almost done

--- AKosireddyIM.c
+++ AKosireddyIM.c
@@ -6,22 +6,25 @@
 #include "AKosireddyIM.h"
 #define BUFFERSIZE 100
 
-int all_paths(int start_city, int current_city, int end_city, int** adj_matrix, int n);
-int* fastest_paths(int** path_matrix);
+int fastest_path(int start_city, int end_city, int** adj_matrix, int n);
+int search(int start_city, int current_city, int end_city, int** adj_matrix, int n, int prev_time);
 int main(int argc, char* argv[])
 {
     char buffer[BUFFERSIZE];
     const int n = atoi(fgets(buffer, BUFFERSIZE, stdin));
     int** adj_matrix = (int**)malloc(n * sizeof(int*));
-    for (int i = 0; i < n; ++i) adj_matrix[i] = (int*)malloc((n - 1) * sizeof(int));
+    for (int i = 0; i < n; ++i) adj_matrix[i] = (int*)malloc((i + 1) * sizeof(int));
+    for (int i = 0; i < n; ++i)
+        for (int j = 0; j < n; j++)
+            adj_matrix[i][j] = INT_MAX;
     int i = 0;
     int j = 0;
     //int i;
     while (fgets(buffer, BUFFERSIZE, stdin)) {
         char* token = strtok(buffer, " \n");
         while (token != NULL) {
-            if (strcmp(token, "x") == 0) adj_matrix[i][j] = -1;
-            else adj_matrix[i][j] = atoi(token);
+            if (strcmp(token, "x") != 0)
+                adj_matrix[i][j] = atoi(token);
             token = strtok(NULL, " \n");
             printf("%d ", adj_matrix[i][j]);
             ++j;
@@ -31,68 +34,41 @@ int main(int argc, char* argv[])
         ++i;
     }
 
-    int minimum_times = all_paths(1, 1, 2, adj_matrix, n);
-    /*for (int path_row = 0; path_row < n; ++path_row) {
-        for (int path_col = 0; path_col < n; ++path_col) {
-            printf("%d ", path_matrix[path_row][path_col]);
-        }
-        printf('\n');
-    }*/
-
+    int minimum_times = fastest_path(1, 3, adj_matrix, n);
     return 0;
 }
 
-int all_paths(int start_city, int current_city, int end_city, int** adj_matrix, int n) {
-    if (current_city > n) return INT_MAX;
-    int* times = malloc(n * sizeof(int));
-    for (int i = 0; i < n; ++i) times[i] = INT_MAX;
-    static bool visited[300];
-    visited[start_city - 1] = true;
-    if (current_city != end_city)visited[current_city - 1] = true;
-    int* queue = malloc(n * sizeof(int));
-    int i = 0;
-    for (; i < n; ++i) {
-        if (!visited[i] && current_city != end_city) {
-            if (times[i] == INT_MAX) times[i] = 0;
-            times[i] += all_paths(start_city + 1, i + 1, end_city, adj_matrix, n);
-        }
-    }
-    int min_time = INT_MAX;
-    if (current_city == end_city) return adj_matrix[current_city - 2][end_city - 2];
-    if (current_city > 1) return adj_matrix[current_city - 2][end_city - 1] + adj_matrix[current_city - 2][start_city - 2];
-    else if (current_city == 1) {
-        for (int i = 0; i < n; ++i) {
-            if (times[i] < min_time && times[i] > -1) {
-                min_time = times[i];
-            }
-        }
-    }
-    else return min(adj_matrix[current_city - 2][end_city - 1], all_paths(start_city, current_city + 1, end_city, adj_matrix, n));
-
-    return min_time;
+int fastest_path(int start_city, int end_city, int** adj_matrix, int n) {
+    return search(start_city, start_city + 1, end_city, adj_matrix, n, 0);
 }
 
-/*int** all_paths(int start_city, int end_city, int** adj_matrix) {
-    int length = sizeof(adj_matrix) / 2;
-    int** path_matrix = (int**)malloc(sizeof(adj_matrix) * sizeof(int*));
-    for (int i = 0; i < length; ++i) path_matrix[i] = (int*)malloc(length * sizeof(int));
-    int stops_offset = 1;
-    int first_stop_offset = 3;
-    for (int stops = 0; stops < length; ++stops) {
-        path_matrix[stops][current_city] = adj_matrix[current_city][0];
-    //    printf("%d ", path_matrix[0][current_city]);
+int search(int start_city, int current_city, int end_city, int** adj_matrix, int n, int prev_time) {
+    if (current_city == end_city) {
+        if (current_city < start_city)
+            return adj_matrix[start_city - 2][current_city - 1] + prev_time;
+        else
+            return adj_matrix[current_city - 2][start_city - 1] + prev_time;
     }
-    for (int path_col = 1; path_col < length; ++path_col) {
-        for (int path_row = 1; path_row < length; ++path_row) {
-            path_matrix[path_row][path_col] = path_matrix[path_row - 1][path_col] + adj_matrix[path_row][path_col];
+    int* time = calloc(n, sizeof(int));
+    static bool searched[300];
+    for (int i = 0; i < n; ++i) {
+        if (!(searched[i] || current_city == i + 1)) {
+            searched[current_city - 1] = true;
+            if(current_city > 1)
+                if (i > 0 && current_city <= i)
+                    time[i] = min(search(current_city, i + 1, end_city, adj_matrix, n, adj_matrix[i - 1][current_city - 1]), adj_matrix[current_city - 2][end_city - 1]);
+                else
+                    time[i] = min(search(current_city, i + 1, end_city, adj_matrix, n, adj_matrix[current_city - 2][i]), adj_matrix[current_city - 2][end_city - 1]);
+            else
+                time[i] = min(search(current_city, i + 1, end_city, adj_matrix, n, adj_matrix[i - 1][current_city - 1]), adj_matrix[end_city - 2][current_city - 1]);
         }
     }
-
-    return path_matrix;
-}*/
-
-int* fastest_paths(int** path_matrix) {
-    int length = sizeof(path_matrix) / sizeof(int*);
-    int* fastest_paths = (int*)malloc(length * sizeof(int));
-    return fastest_paths;
+    searched[current_city - 1];
+    int min_time = INT_MAX;
+    for (int i = 0; i < n; ++i) {
+        if (time[i] < min_time)
+            if (time[i] > 0 || min_time < 0)
+                min_time = time[i];
+    }
+    return min_time + prev_time;
 }
